2a169b0f91fd624cd9b47e63c95ccde54e517233
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-11-04 15:28:57 +0700

    Refactor SSHClient import and add backup password option

--- disk_usage_checking.py
+++ disk_usage_checking.py
@@ -2,7 +2,7 @@ import os
 import json
 import time
 import schedule
-from paramiko import SSHClient, MissingHostKeyPolicy, AuthenticationException, SSHException
+from paramiko import SSHClient, AutoAddPolicy, AuthenticationException, SSHException
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -22,31 +22,26 @@ def main():
         if validate_password(site['ip'], DEFAULT_PASSWORD):
             remote_main(site['ip'], DEFAULT_PASSWORD)
         else:
-            print("Default password failed. Trying using raspberry password.")
+            print("Default password failed. Trying using backup password.")
             if validate_password(site['ip'], BACKUP_PASSWORD):
-                print("Using raspberry password.")
+                print("Using backup password.")
                 remote_main(site['ip'], BACKUP_PASSWORD)
             else:
-                print("Failed to connect with both passwords. Please check the device or VPN Bakti Connection.\n")
-
+                print("Failed to connect with both passwords. Please check the device or VPN connection.\n")
 
 def validate_password(ip, password):
-    try:
-        with create_ssh_client(ip, password) as ssh:
-            print("Connection successful with current password.\n")
-            return True
-    except AuthenticationException:
-        print("Authentication failed.")
-    except SSHException as e:
-        print(f"SSH Error: {e}")
-    except Exception as e:
-        print(f"Unexpected error: {e}")
-    return False
-
+    ssh = create_ssh_client(ip, password)
+    if ssh:
+        print("Connection successful with current password.\n")
+        ssh.close()
+        return True
+    else:
+        return False
 
 def remote_main(ip, password):
-    try:
-        with create_ssh_client(ip, password) as ssh:
+    ssh = create_ssh_client(ip, password)
+    if ssh:
+        try:
             # Check disk usage
             print("---- Checking Disk Usage ----")
             stdin, stdout, stderr = ssh.exec_command("df -h")
@@ -63,26 +58,36 @@ def remote_main(ip, password):
                     ssh.exec_command("sudo reboot")
                 else:
                     print("Disk usage is below 70%.\n")
-            
+
             except IndexError:
                 print("Failed to parse disk usage output.")
 
-    except SSHException as e:
-        print(f"SSH command failed: {e}")
-    except Exception as e:
-        print(f"Unexpected error: {e}")
-    print("\n")
-
+        except SSHException as e:
+            print(f"SSH command failed: {e}")
+        finally:
+            ssh.close()
+    else:
+        print("SSH connection failed.\n")
 
 def create_ssh_client(ip, password):
     ssh = SSHClient()
-    ssh.set_missing_host_key_policy(MissingHostKeyPolicy())
-    ssh.load_system_host_keys()
-    ssh.connect(ip, username=SSH_USER, password=password)
-    return ssh
+    ssh.set_missing_host_key_policy(AutoAddPolicy())
+    try:
+        ssh.connect(ip, username=SSH_USER, password=password)
+        return ssh
+    except AuthenticationException:
+        print("Authentication failed.")
+    except SSHException as e:
+        print(f"SSH Error: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+    return None
+
+print("Scheduler started.")
 
 # Schedule the main function to run every 6 hours
 schedule.every(6).hours.do(main)
+# schedule.every(1).minutes.do(main)
 
 # Run the scheduled tasks
 while True:
